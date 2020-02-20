d5869312edebafccafffe2257f72c1e3d777fefa
katherinekimes <57684996+katherinekimes@users.noreply.github.com>
2020-02-20 19:54:35 +0000

    Calculations Working/Displaying Correctly

--- src/index.ts
+++ src/index.ts
@@ -9,32 +9,23 @@ const buttons = document.querySelectorAll('.btn') as NodeListOf<HTMLButtonElemen
 const tenButton = document.getElementById('tenButton') as HTMLButtonElement;
 const fifteenButton = document.getElementById('fifteenButton') as HTMLButtonElement;
 const twentyButton = document.getElementById('twentyButton') as HTMLButtonElement;
+const tipPercent = document.getElementById('tipPercent') as HTMLSpanElement;
 
 tenButton.addEventListener('click', handleClick);
 fifteenButton.addEventListener('click', handleClick);
 twentyButton.addEventListener('click', handleClick);
 
 function handleClick() {
-    console.log('Hello');
+    displayBillAmount.innerText = (parseFloat(inputBillAmount.value).toFixed(2).toString());
     displayTipPercent.innerText = this.innerText;
+    tipPercent.innerText = this.innerText;
+    const percent = parseInt(displayTipPercent.innerText);
+    let bill = parseFloat(inputBillAmount.value);
+    let tip = bill * (percent/100);
+    let total = tip + bill;
+    displayTipAmount.innerText = tip.toFixed(2).toString();
+    displayTotal.innerText = total.toFixed(2).toString();
 }
-   // const that = this as HTMLButtonElement;
-    //let percent: number = parseInt(that.innerText.toString());
-    //displayTipPercent.innerText = percent.toString();
-   // that.classList.add('selected');
-   // that.removeEventListener('click', handleClick);
 
-   // buttons.forEach(btn => {
-   //     if (btn !== that) {
-   //         btn.classList.add('deselected');
-      //  }
-     //   else {
-       //     that.classList.add('deselected');
-       //     that.removeEventListener('click', handleClick);
-     //   }
-    //});
-//}
 
-function updateAmounts() {
-    displayBillAmount.innerText = inputBillAmount.innerText;
-}
+
